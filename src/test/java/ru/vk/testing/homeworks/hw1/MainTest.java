a1ab658c9b4726dc90da5b1c1f936b59d83c20b9
max <golchanskiy23@gmail.com>
2025-03-19 06:23:12 +0300

    Solved HW1, wrote AssertTrue test for main()

---
+++ src/test/java/ru/vk/testing/homeworks/hw1/MainTest.java
@@ -0,0 +1,13 @@
+package ru.vk.testing.homeworks.hw1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue("Hello world",true);
+    }
+}
